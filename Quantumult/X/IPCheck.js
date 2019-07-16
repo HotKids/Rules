0c0dcfe2d4d64316aa71659e67d3b7048c375c15
JO2EY <14944603+JO2EY@users.noreply.github.com>
2019-07-16 15:01:40 +0800

    🐳

--- Quantumult/X/IPCheck.js
+++ Quantumult/X/IPCheck.js
@@ -22,7 +22,7 @@ var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "�
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = flags.get(obj['country_iso']) + ' '+ obj['country'];
-var subtitle = ValidCheck(obj['city'])+' - ['+obj['asn_org']+']';
+var subtitle = ValidCheck(obj['city'])+' - '+obj['asn_org'];
 var ip = obj['ip'];
 var description = obj['country'] + '-' +ValidCheck(obj['city']) + '\n' + obj['asn'] + '\n' + obj['asn_org']+ '\n' + obj['ip'];
 
