9a3d13f7db66e66b0c59db669d9430c02a168e1a
JO2EY <14944603+JO2EY@users.noreply.github.com>
2019-07-26 09:59:28 +0800

    🐳

---
+++ Script/Poizon.js
@@ -0,0 +1,18 @@
+let result = JSON.parse(body);
+
+delete result.data.boot;
+delete result.data.bootAnimation;
+
+JSON.stringify(result);
+
+/**********************************************************
+毒去广告
+Surge
+[Script]
+http-request ^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ script-path=https://raw.githubusercontent.com/JO2EY/Rules/master/Script/115tonPlayer.js,requires-body=true
+[MITM]
+hostname = m.poizon.com
+
+Quantumult X
+^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ url script-response-body https://raw.githubusercontent.com/JO2EY/Rules/master/Script/115tonPlayer.js
+**********************************************************/
