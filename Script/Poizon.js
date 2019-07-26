ed0245882f58996ab0518e53a01534016b7e30c4
JO2EY <14944603+JO2EY@users.noreply.github.com>
2019-07-26 10:07:50 +0800

    Update Poizon.js

--- Script/Poizon.js
+++ Script/Poizon.js
@@ -9,10 +9,11 @@ JSON.stringify(result);
 毒去广告
 Surge
 [Script]
-http-request ^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ script-path=https://raw.githubusercontent.com/JO2EY/Rules/master/Script/Poizon.js,requires-body=true
+http-request ^https?:\/\/m\.poizon\.com\/client\/init script-path=https://raw.githubusercontent.com/JO2EY/Rules/master/Script/Poizon.js,requires-body=true
 [MITM]
 hostname = m.poizon.com
 
 Quantumult X
-^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ url script-response-body https://raw.githubusercontent.com/JO2EY/Rules/master/Script/Poizon.js
+hostname = m.poizon.com
+^https?:\/\/m\.poizon\.com\/client\/init url script-response-body https://raw.githubusercontent.com/JO2EY/Rules/master/Script/Poizon.js
 **********************************************************/
