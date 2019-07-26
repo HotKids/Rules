f4bb494e34683c373b7287965f3ad0df939cb4a6
JO2EY <14944603+JO2EY@users.noreply.github.com>
2019-07-26 09:57:11 +0800

    Update 115tonPlayer.js

--- Script/115tonPlayer.js
+++ Script/115tonPlayer.js
@@ -1,10 +1,8 @@
-/*
-提取115中的视频使用 nPlayer 进行播放.
-使用方法：
-1.在[Script]分组下添加下面这行配置
-http-request ^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ script-path=https://raw.githubusercontent.com/JO2EY/Rules/master/Script/115tonPlayer.js
-^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ url script-response-body https://raw.githubusercontent.com/JO2EY/Rules/master/Script/115tonPlayer.js
-*/
-
 $notify('播放地址提取成功, 长按(重按)通知查看', '',  'nplayer-' + $request.url);
 $done({});
+
+/**********************************************************
+提取115中的视频使用 nPlayer 进行播放
+
+^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ url script-response-body https://raw.githubusercontent.com/JO2EY/Rules/master/Script/115tonPlayer.js
+**********************************************************/
