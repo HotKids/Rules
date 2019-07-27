6c346cb259c887ff541c21a9eac5bb14ee0708ce
JO2EY <14944603+JO2EY@users.noreply.github.com>
2019-07-27 21:40:05 +0800

    Update Poizon.js

--- Script/Poizon.js
+++ Script/Poizon.js
@@ -12,8 +12,4 @@ Surge
 http-request ^https?:\/\/m\.poizon\.com\/client\/init script-path=https://raw.githubusercontent.com/JO2EY/Rules/master/Script/Poizon.js,requires-body=true
 [MITM]
 hostname = m.poizon.com
-
-Quantumult X
-hostname = m.poizon.com
-^https?:\/\/m\.poizon\.com\/client\/init url script-response-body https://raw.githubusercontent.com/JO2EY/Rules/master/Script/Poizon.js
 **********************************************************/
