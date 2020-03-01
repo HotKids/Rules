ac5497877ac08d87dd795ac7e1f71372243ebf66
JO2EY <14944603+JO2EY@users.noreply.github.com>
2020-03-01 21:29:20 +0800

    🐳

--- Script/eval_script.js
+++ Script/eval_script.js
@@ -1,14 +1,21 @@
 /**
- * 远程脚本管理（QuanX 举例，Surge 同理）
+ * 远程脚本管理（QuanX 举例）
  * 
- * 1.设置定时任务更新添加的远程脚本，第一次运行需要手动执行一下更新脚本（ Qanx 普通调试模式容易更新失败，使用最新 TF 红色按钮调试），例如设置每天凌晨更新脚本：
+ * 1.设置定时任务更新添加的远程脚本，第一次运行需要手动执行一下更新脚本（Qanx 普通调试模式容易更新失败，使用最新 TF 红色按钮调试），例如设置每天凌晨更新脚本：
  * [task_local]
  * 0 0 * * * eval_script.js
  * 
  * 2.__conf 配置说明：
- * 参考下面 __conf 对象，key = 远程脚本的 URL，value = 匹配脚本对应的 URL
  * 
- * 3.修改配置文件的本地脚本为此脚本，例如之前京东 jd_price.js 改为 eval_script.js 即可：
+ * 参考下面 __conf 示例
+ * 
+ * [远程订阅脚本]
+ * 参考示例：https://raw.githubusercontent.com/yichahucha/surge/master/sub_script.conf
+ * 
+ * [本地添加脚本远程连接]
+ * 格式为：远程脚本的链接 url 匹配脚本对应的正则1,匹配脚本对应的正则2
+ * 
+ * 如果是本地添加需要修改配置文件的本地脚本为此脚本，例如之前京东 jd_price.js 改为 eval_script.js 即可：
  * [rewrite_local]
  * # ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig) url script-response-body jd_price.js
  * ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig) url script-response-body eval_script.js
@@ -16,30 +23,28 @@
  * hostname = api.m.jd.com
  */
 
-const __conf = {
-    "https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js": "^https?:\/\/api\.m\.jd.com",
-    "https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js": ["^https?:\/\/trade-acs\.m\.taobao\.com", "^https?:\/\/amdc\.m\.taobao\.com"],
-    "https://raw.githubusercontent.com/yichahucha/surge/master/nf_rating.js": "^https?:\/\/ios\.prod\.ftl\.netflix\.com",
-    "https://raw.githubusercontent.com/yichahucha/surge/master/wb_ad.js": "^https?:\/\/m?api\.weibo\.c(n|om)",
-    "https://raw.githubusercontent.com/yichahucha/surge/master/wb_launch.js": "^https?:\/\/(sdk|wb)app\.uve\.weibo\.com",
-    "https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js": "^https?:\/\/mp\.weixin\.qq\.com",
-    "https://raw.githubusercontent.com/primovist/ScriptsForSurge/master/Scripts/Bilibili.js": "^https?:\/\/ap(i|p)\.bilibili\.com",
-    "https://raw.githubusercontent.com/Choler/Surge/master/Script/Aweme.js": "^https?:\/\/.*\.amemv\.com",
-    "https://raw.githubusercontent.com/Choler/Surge/master/Script/Super.js": "^https?:\/\/[a-z]*\.snssdk\.com",
-    "https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bahamut.js": "^https?:\/\/api\.gamer\.com",
-    "https://raw.githubusercontent.com/primovist/ScriptsForSurge/master/Scripts/DingXiangDrugs.js": "^https?:\/\/(i|newdrugs)\.dxy\.cn",
-    "https://raw.githubusercontent.com/eHpo1/Surge/master/Script/wps.js": "^https?:\/\/account\.wps\.cn",
-    "https://raw.githubusercontent.com/langkhach270389/Scripting/master/CamScaner.js": "^https?:\/\/(api|api-cs)\.intsig\.net",
-    "https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js": "^https?:\/\/api\.(picsart|meiease)\.c(n|om)",
-    "https://raw.githubusercontent.com/langkhach270389/Scripting/master/musixmatch.miao.js": "^https?:\/\/apic\.musixmatch\.com",
-    "https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js": "^https?:\/\/api\.vuevideo\.net",
-    "https://raw.githubusercontent.com/langkhach270389/Scripting/master/documents.js": "^https?:\/\/license\.pdfexpert\.com",
-    "https://raw.githubusercontent.com/langkhach270389/Scripting/master/Pdfexpert.vip.js": "^https?:\/\/license\.pdfexpert\.com"
-}
 
-const __tool = new __Tool()
+//配置
+const __conf = String.raw`
+
+
+
+[remote]
+//配置脚本远程订阅
+https://raw.githubusercontent.com/JO2EY/Rules/master/Quantumult/X/Script_eval.conf
+https://raw.githubusercontent.com/JO2EY/Rules/master/Quantumult/X/GetCookie_eval.conf
+
+[local]
+//配置脚本远程连接
+//京东
+//https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js url ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig)
+
+
+
+`
+
+const __tool = new ____Tool()
 const __isTask = __tool.isTask
-
 if (__isTask) {
     const downloadScript = (url) => {
         return new Promise((resolve) => {
@@ -48,96 +53,178 @@ if (__isTask) {
                 if (!error) {
                     if (response.statusCode == 200) {
                         __tool.write(body, url)
-                        resolve(`🪓${filename} update success`)
+                        resolve({ body, msg: `🪓${filename} update success` })
                         console.log(`Update success: ${url}`)
                     } else {
-                        resolve(`🪓${filename} update fail`)
+                        resolve({ body, msg: `🪓${filename} update fail` })
                         console.log(`Update fail ${response.statusCode}: ${url}`)
                     }
                 } else {
-                    resolve(`🪓${filename} update fail`)
+                    resolve({ body: null, msg: `🪓${filename} update fail` })
                     console.log(`Update fail ${error}: ${url}`)
                 }
             })
         })
     }
-    const promises = (() => {
-        let all = []
-        Object.keys(__conf).forEach((url) => {
-            all.push(downloadScript(url))
-        });
-        return all
-    })()
-    console.log("Start updating...")
-    Promise.all(promises).then(vals => {
-        console.log("Stop updating.")
-        console.log(vals.join("\n"))
-        let lastDate = __tool.read("ScriptLastUpdateDate")
-        lastDate = lastDate ? lastDate : new Date().Format("yyyy-MM-dd HH:mm:ss")
-        __tool.notify("Update done.", `${lastDate} last update.`, `${vals.join("\n")}`)
-        __tool.write(new Date().Format("yyyy-MM-dd HH:mm:ss"), "ScriptLastUpdateDate")
-        $done()
+
+    const getConf = (() => {
+        return new Promise((resolve) => {
+            const remoteConf = ____removeGarbage(____getConfInfo(__conf, "remote"))
+            const localConf = ____removeGarbage(____getConfInfo(__conf, "local"))
+            if (remoteConf.length > 0) {
+                const promises = (() => {
+                    let all = []
+                    remoteConf.forEach((url) => {
+                        all.push(downloadScript(url))
+                    })
+                    return all
+                })()
+                Promise.all(promises).then(result => {
+                    let allRemoteConf = ""
+                    let allRemoteMSg = ""
+                    result.forEach(data => {
+                        if (data.body) {
+                            allRemoteConf += "\n" + ____parseRemoteConf(data.body)
+                        }
+                        allRemoteMSg += allRemoteMSg.length > 0 ? "\n" + data.msg : data.msg
+                    });
+                    let content = localConf.join("\n")
+                    if (allRemoteConf.length > 0) {
+                        content = `${content}\n${allRemoteConf}`
+                    }
+                    resolve({ content, msg: allRemoteMSg })
+                })
+            } else {
+                const content = localConf.join("\n")
+                resolve({ content: content, msg: "" })
+            }
+        })
     })
+    
+    getConf()
+        .then((conf) => {
+            const parseConf = ____parseConf(conf.content)
+            const promises = (() => {
+                let all = []
+                Object.keys(parseConf).forEach((url) => {
+                    all.push(downloadScript(url))
+                })
+                return all
+            })()
+            console.log("Start updating...")
+            Promise.all(promises).then(result => {
+                console.log("Stop updating.")
+                const notifyMsg = (() => {
+                    let msg = conf.msg
+                    result.forEach(data => {
+                        msg += msg.length > 0 ? "\n" + data.msg : data.msg
+                    });
+                    return msg
+                })()
+                console.log(notifyMsg)
+                let lastDate = __tool.read("ScriptLastUpdateDate")
+                lastDate = lastDate ? lastDate : new Date().Format("yyyy-MM-dd HH:mm:ss")
+                __tool.notify("Update Done.", `${lastDate} last update.`, `${notifyMsg}`)
+                __tool.write(JSON.stringify(parseConf), "ScriptConfObject")
+                __tool.write(new Date().Format("yyyy-MM-dd HH:mm:ss"), "ScriptLastUpdateDate")
+                $done()
+            })
+        })
 }
 
 if (!__isTask) {
     const __url = $request.url
+    const __confObj = JSON.parse(__tool.read("ScriptConfObject"))
     const __script = (() => {
         let s = null
-        for (let key in __conf) {
-            let value = __conf[key]
-            if (Array.isArray(value)) {
-                value.some((item) => {
-                    if (__url.match(item)) {
-                        s = { url: key, content: __tool.read(key) }
-                        return true
-                    }
-                })
-            } else {
-                if (__url.match(value)) {
-                    s = { url: key, content: __tool.read(key) }
+        for (let key in __confObj) {
+            let value = __confObj[key]
+            if (!Array.isArray(value)) value = value.split(",")
+            value.some((url) => {
+                if (__url.match(url)) {
+                    s = { url: key, content: __tool.read(key), match: url }
+                    return true
                 }
-            }
+            })
         }
         return s
     })()
     if (__script) {
         if (__script.content) {
             eval(__script.content)
-            console.log(`Execute script: ${__script.url}`)
+            console.log(`Request url: ${__url}\nMatch url: ${__script.match}\nExecute script: ${__script.url}`)
         } else {
             $done({})
-            console.log(`Not found script: ${__script.url}`)
+            console.log(`Request url: ${__url}\nMatch url: ${__script.match}\nScript not executed. Script not found: ${__script.url}`)
         }
     } else {
         $done({})
-        console.log(`Not match URL: ${__url}`)
+        console.log(`No match url: ${__url}`)
     }
 }
 
-if (!Array.isArray) {
-    Array.isArray = function (arg) {
-        return Object.prototype.toString.call(arg) === '[object Array]'
+function ____getConfInfo(conf, type) {
+    const rex = new RegExp("\\[" + type + "\\](.|\\n)*?($|\\n\\[)", "g")
+    let result = rex.exec(conf)
+    result = result[0].split("\n")
+    if (result[2].length > 0) {
+        result.pop()
     }
+    result.shift()
+    return result
 }
 
-Date.prototype.Format = function (fmt) {
-    var o = {
-        "M+": this.getMonth() + 1,
-        "d+": this.getDate(),
-        "H+": this.getHours(),
-        "m+": this.getMinutes(),
-        "s+": this.getSeconds(),
-        "q+": Math.floor((this.getMonth() + 3) / 3),
-        "S": this.getMilliseconds()
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
+function ____parseRemoteConf(conf) {
+    const lines = conf.split("\n")
+    let newLines = []
+    lines.forEach((line) => {
+        line = line.replace(/^\s*/, "")
+        if (line.length > 0 && line.substring(0, 3) == "###") {
+            line = line.replace("###", "")
+            line = line.replace(/^\s*/, "")
+            newLines.push(line)
+        }
+    })
+    return newLines.join("\n")
 }
 
-function __Tool() {
+function ____removeGarbage(lines) {
+    let newLines = []
+    lines.forEach((line) => {
+        line = line.replace(/^\s*/, "")
+        if (line.length > 0 && line.substring(0, 2) != "//") {
+            newLines.push(line)
+        }
+    })
+    return newLines
+}
+
+function ____parseConf(conf) {
+    const lines = conf.split("\n")
+    let confObj = {}
+    lines.forEach((line) => {
+        line = line.replace(/^\s*/, "")
+        if (line.length > 0 && line.substring(0, 2) != "//") {
+            const avaliable = (() => {
+                const format = /^https?:\/\/.*\s+url\s+.*/
+                return format.test(line)
+            })()
+            if (avaliable) {
+                const value = line.split("url")
+                const remote = value[0].replace(/\s/g, "")
+                const match = value[1].replace(/\s/g, "")
+                confObj[remote] = match
+            } else {
+                __tool.notify("Configuration error", "", line)
+                throw "Configuration error:" + line
+            }
+        }
+    })
+    console.log(`Configuration information:  ${JSON.stringify(confObj)}`)
+    return confObj
+}
+
+function ____Tool() {
     _node = (() => {
         if (typeof require == "function") {
             const request = require('request')
@@ -196,4 +283,26 @@ function __Tool() {
         }
         return response
     }
+}
+
+if (!Array.isArray) {
+    Array.isArray = function (arg) {
+        return Object.prototype.toString.call(arg) === '[object Array]'
+    }
+}
+
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "H+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S": this.getMilliseconds()
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
 }
