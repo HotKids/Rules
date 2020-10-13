c02ab4d7d2255400e8786fedc1770d60de940b5f
旺仔 <14944603+HotKids@users.noreply.github.com>
2020-10-13 16:36:14 +0800

    🐳

---
+++ Surge/xiami.js
@@ -0,0 +1,18 @@
+const $ = new cmp()
+
+let checkin = "xiami://"
+
+$.notify("虾米", "", "虾米音乐每日签到", checkin)
+
+$done()
+
+function cmp() {
+    _isQuanX = typeof $task != "undefined"
+    _isLoon = typeof $loon != "undefined"
+    _isSurge = typeof $httpClient != "undefined" && !_isLoon
+    this.notify = (title, subtitle, message, url) => {
+        if (_isLoon) $notification.post(title, subtitle, message, url)
+        if (_isQuanX) $notify(title, subtitle, message, { "open-url": url })
+        if (_isSurge) $notification.post(title, subtitle, message, { url: url })
+    }
+}
