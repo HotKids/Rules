096705a4f2eef3f1ba8f324803bbff15977355b7
旺仔 <14944603+HotKids@users.noreply.github.com>
2020-11-09 10:47:35 +0800

    Update weixin110.js

--- Script/weixin110.js
+++ Script/weixin110.js
@@ -20,16 +20,16 @@ hostname= weixin110.qq.com
 
 var str = ($response.body);
 
-str = str.match(/:&#x2f;&#x2f;(\S*)"}/)[1].replace(/&#x2f;/g, '/').replace(/&amp;/g, '&')
+str = str.match(/:&#x2f;&#x2f;(\S*)"}/)[1].replace(/&#x2f;/g, '/').replace(/&amp;/g, '&').split("\"")[0]
 let opener = str.indexOf("m.tb.cn") != -1 ? "taobao://" + str: ($response.body)
 //console.log(str);
 
 const $ = new cmp()
 
 if (str.indexOf("m.tb.cn") != -1) {
-    $.notify(``, "👽去你大爷的内置浏览器", "🛍️点击打开淘宝", opener)
+    $.notify(``, "", "🛍️点击打开淘宝", opener)
 } else if (str.indexOf("如需浏览")) {
-    $.notify(``, "👽去你大爷的内置浏览器", "🔗点击打开链接", "https://"+str)
+    $.notify(``,"", "🔗点击打开链接", "https://"+str)
 }
 
 $done({body: $response.body});
